f867a68172debe7c7d226009f7a7b4b899b447f3
Drew Beller <drew@spanninglabs.com>
2022-09-08 17:00:51 -0700

    update deploy

--- demo_app/constants/nftContract.ts
+++ demo_app/constants/nftContract.ts
@@ -9,7 +9,7 @@ export const nftAbi = jadeNFTJson.abi;
 // single chain. We only need to read data from the one source of truth contract
 // on the Avalanche Fuji Network.
 export const nftSettlementChain = networkIds.get("Avalanche Fuji")!;
-export const nftLegacyAddress = "0x8b6203dd90DDb61251baE83B40ad4822d1BB582d";
+export const nftLegacyAddress = "0xe909E233c7fB9eDf137a9C561aBAE29156b5eC02";
 export const nftAddress = getSpanningAddress(
   parseInt(nftSettlementChain),
   nftLegacyAddress
